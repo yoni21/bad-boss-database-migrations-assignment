35b0431b4fbe255e750865debaa53f3fa2373f58
yonas samuel <yonastlolo@yahoo.com>
2017-02-02 13:42:57 -0500

    adding quantity

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202181313) do
+ActiveRecord::Schema.define(version: 20170202183157) do
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
     t.string  "manufacturer"
     t.decimal "cost"
+    t.integer "quantity"
   end
 
 end
